6e7cb23ce30669da0b32771c710d17fe5c20e08e
mirsal <mirsal@9e24bf6e-2ee3-4532-97b1-3e928c6f3323>
2009-09-15 00:12:46 +0000

    Bootstrap the frontend app and its html5 layout
    git-svn-id: http://svn.idci-consulting.fr/citadin.fr/trunk@5 9e24bf6e-2ee3-4532-97b1-3e928c6f3323

---
+++ apps/frontend/templates/layout.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <?php include_http_metas() ?>
+    <?php include_metas() ?>
+    <?php include_title() ?>
+    <link rel="shortcut icon" href="/favicon.ico" />
+
+<!--[if IE]>
+	<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+<!--[if lte IE 7]>
+	<script src="http://ie7-js.googlecode.com/svn/version/2.0(beta3)/IE8.js" type="text/javascript"></script><![endif]-->
+</head>
+
+<body>
+	<header>
+		<div class="wrapper">
+			<figure class="logo">Citad'in</figure>
+			<nav><ol>
+				<li><a href="#">Accueil</a></li>
+				<li><a href="#">Recherche manuelle</a></li>
+				<li><a href="#">Recherche assistée</a></li>
+				<li><a href="#">Contact</a></li>
+				<li><form><input type="text" /></form></li>
+			</ol></nav>
+		</div>
+	</header>
+    <?php echo $sf_content ?>
+    <footer>
+    	<h2>Derniers ajouts</h2>
+    	<article>
+    		<figure class="thumbnail"></figure>
+    		<h3>T3</h3>
+    		<meter class="surface">75m2</meter>
+    		<address class="location">LYON 03</address>
+    	</article>
+    </footer>
+</body>
+</html>
