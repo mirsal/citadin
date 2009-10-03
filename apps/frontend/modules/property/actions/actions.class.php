9a5a3321ab383704013b34fceb49aa82ad642b0e
gabriel <gabriel@9e24bf6e-2ee3-4532-97b1-3e928c6f3323>
2009-10-03 08:34:34 +0000

    work in progress for the schema
    git-svn-id: http://svn.idci-consulting.fr/citadin.fr/trunk@25 9e24bf6e-2ee3-4532-97b1-3e928c6f3323

---
+++ apps/frontend/modules/property/actions/actions.class.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * property actions.
+ *
+ * @package    citadin
+ * @subpackage property
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 12479 2008-10-31 10:54:40Z fabien $
+ */
+class propertyActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+  }
+}
