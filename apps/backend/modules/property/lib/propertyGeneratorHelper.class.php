c83e6fdd08da3ef317d515b7d880da636f7dcede
mirsal <mirsal@9e24bf6e-2ee3-4532-97b1-3e928c6f3323>
2010-01-21 04:12:09 +0000

    Add a property admin module
    git-svn-id: http://svn.idci-consulting.fr/citadin.fr/trunk@34 9e24bf6e-2ee3-4532-97b1-3e928c6f3323

---
+++ apps/backend/modules/property/lib/propertyGeneratorHelper.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * property module helper.
+ *
+ * @package    citadin
+ * @subpackage property
+ * @author     IIWI Studios
+ * @version    SVN: $Id: helper.php 12474 2008-10-31 10:41:27Z fabien $
+ */
+class propertyGeneratorHelper extends BasePropertyGeneratorHelper
+{
+}
