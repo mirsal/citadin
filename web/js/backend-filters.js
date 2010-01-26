ad880b3feb9f78942f2059d1a654ef37e6608a14
mirsal <mirsal@9e24bf6e-2ee3-4532-97b1-3e928c6f3323>
2010-01-26 11:37:18 +0000

    Hide the backend filters and add a quick and dirty toggle link
    git-svn-id: http://svn.idci-consulting.fr/citadin.fr/trunk@45 9e24bf6e-2ee3-4532-97b1-3e928c6f3323

---
+++ web/js/backend-filters.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('.sf_admin_filter').hide().closest('#sf_admin_container').find('h1')
+        .append('<a href="#" class="toggle-filters">Filter results</a>');
+    
+    $('a.toggle-filters').click(function(ev) {
+        $(this).closest('#sf_admin_container').find('.sf_admin_filter').slideToggle();
+        ev.preventDefault();
+    });
+});
