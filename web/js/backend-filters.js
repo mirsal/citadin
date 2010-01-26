aa7ee05968adf0d79e94b452edc56167676b3c6e
mirsal <mirsal@9e24bf6e-2ee3-4532-97b1-3e928c6f3323>
2010-01-26 23:39:14 +0000

    Do not hide the filters when they contain errors
    git-svn-id: http://svn.idci-consulting.fr/citadin.fr/trunk@52 9e24bf6e-2ee3-4532-97b1-3e928c6f3323

--- web/js/backend-filters.js
+++ web/js/backend-filters.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $('.sf_admin_filter').hide().closest('#sf_admin_container').find('h1')
+    $('.sf_admin_filter:not(:has(ul.error_list))').hide().closest('#sf_admin_container').find('h1')
         .append('<a href="#" class="toggle-filters">Filter results</a>');
     
     $('a.toggle-filters').click(function(ev) {
