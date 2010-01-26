4206072d685892bdec8397d291331ba731202882
mirsal <mirsal@9e24bf6e-2ee3-4532-97b1-3e928c6f3323>
2010-01-26 18:44:12 +0000

    Quick orientation field filtering proof of concept
    git-svn-id: http://svn.idci-consulting.fr/citadin.fr/trunk@49 9e24bf6e-2ee3-4532-97b1-3e928c6f3323

--- lib/filter/PropertyFormFilter.class.php
+++ lib/filter/PropertyFormFilter.class.php
@@ -14,12 +14,21 @@ class PropertyFormFilter extends BasePropertyFormFilter
   {
     $this->widgetSchema['name'] = new sfWidgetFormFilterInput(array('with_empty' => false));
     $this->widgetSchema['location'] = new sfWidgetFormFilterInput(array('with_empty' => false));
+    $this->widgetSchema['description'] = new sfWidgetFormTextarea();
     
     $this->widgetSchema['type'] = new sfWidgetFormSelectRadio(array(
   		'choices' => array_merge(PropertyPeer::getTypes(), array('TYPE_ANY' => 'Both'))
   	));
   	
+  	$orientations = PropertyPeer::getAllValuesForColumn(PropertyPeer::ORIENTATION);
+  	$this->widgetSchema['orientation'] = new sfWidgetFormSelect(array(
+  		'choices' => array_merge(
+  		    array('ORIENTATION_ANY' => 'Any'),
+  		    array_combine($orientations, $orientations))
+  	));
+  	
   	$this->getWidgetSchema()->setDefault('type', 'TYPE_ANY');
+  	$this->getWidgetSchema()->setDefault('orientation', 'ORIENTATION_ANY');
   	
   	$this->validatorSchema['type'] = new sfValidatorAnd(array(
   		$this->validatorSchema['type'],
@@ -29,6 +38,7 @@ class PropertyFormFilter extends BasePropertyFormFilter
   	));
   	
   	$this->widgetSchema['price'] = new iiwiWidgetFormFilterRange();
+    $this->widgetSchema['surface'] = new iiwiWidgetFormFilterRange();
   }
   
   public function getFields()
@@ -49,6 +59,14 @@ class PropertyFormFilter extends BasePropertyFormFilter
   public function convertTypeValue($value)
   {
     if($value === 'TYPE_ANY')
+        return false;
+    
+    return array('text' => $value);
+  }
+  
+  public function convertOrientationValue($value)
+  {
+    if($value === 'ORIENTATION_ANY')
         return false;
     
     return array('text' => $value);
--- lib/model/PropertyPeer.php
+++ lib/model/PropertyPeer.php
@@ -9,4 +9,19 @@ class PropertyPeer extends BasePropertyPeer
 	{
 		return array (self::TYPE_SALE => "For sale", self::TYPE_RENTAL => "For rent");
 	}
+	
+	public static function getAllValuesForColumn($column, Criteria $criteria = null, PropelPDO $con = null)
+	{
+	    $c = is_null($criteria) ? new Criteria() : clone $criteria;
+	    $c->setDistinct(true);
+	    $c->addSelectColumn($column);
+	    
+	    $stmt = self::doSelectStmt($c);
+	    
+	    $values = array();
+	    while($v = $stmt->fetch())
+	        $values[] = $v[0];
+	    
+	    return $values;
+	}
 }
