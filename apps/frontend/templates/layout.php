113c699eb31996e6a3b5c3eedd46eda59ab7fa83
mirsal <mirsal@9e24bf6e-2ee3-4532-97b1-3e928c6f3323>
2010-03-01 20:07:06 +0000

    Reorganize the manual search form
    git-svn-id: http://svn.idci-consulting.fr/citadin.fr/trunk@91 9e24bf6e-2ee3-4532-97b1-3e928c6f3323

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -49,22 +49,29 @@
 		            <select>
 			            <option>Vente</option>
 			            <option>Location</option>
+			            <option>Peu importe</option>
 		            </select>
 	            </fieldset>
 
 	            <fieldset class="right">
-		            <label>Nombre de pièces</label>
-		            <input type="text" size="3" />
-	            </fieldset>
-
-	            <fieldset class="left">
 		            <label>Situé à</label>
 		            <input type="text" size="12" />
 	            </fieldset>
 
-	            <fieldset class="right">
-		            <label>Nombre de chambres</label>
-		            <input type="text" size="3" />
+	            <fieldset class="topspace left">
+	                <h4>Nombre de pièces</h4>
+		            <label>Min</label>
+		            <input type="text" size="4" />
+		            <label>Max</label>
+		            <input type="text" size="4" />
+	            </fieldset>
+
+	            <fieldset class="topspace right">
+		            <h4>Nombre de chambres</h4>
+		            <label>Min</label>
+		            <input type="text" size="4" />
+		            <label>Max</label>
+		            <input type="text" size="4" />
 	            </fieldset>
 
 	            <fieldset class="topspace left">
