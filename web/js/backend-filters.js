e054db876c9e042e6980ecd017f5562b2df34975
mirsal <mirsal@9e24bf6e-2ee3-4532-97b1-3e928c6f3323>
2010-03-27 12:11:52 +0000

    Style the backend filters toggle button
    git-svn-id: http://svn.idci-consulting.fr/citadin.fr/trunk@99 9e24bf6e-2ee3-4532-97b1-3e928c6f3323

--- web/js/backend-filters.js
+++ web/js/backend-filters.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
     $('.sf_admin_filter:not(:has(ul.error_list))').hide()
         .closest('#sf_admin_container').find('h1')
-        .append('<a href="#" class="toggle-filters">Filter results</a>');
+        .append('<a href="#" class="toggle-filters off">Recherche avancée</a>');
     
     $('a.toggle-filters').click(function(ev) {
-        $(this).closest('#sf_admin_container').find('.sf_admin_filter').slideToggle();
+        $(this).toggleClass('off').closest('#sf_admin_container').find('.sf_admin_filter').slideToggle();
         ev.preventDefault();
     });
 });
