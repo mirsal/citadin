ead94e02d619ae9471da344c80102b9090fe1e02
gabriel <gabriel@9e24bf6e-2ee3-4532-97b1-3e928c6f3323>
2010-03-27 16:55:26 +0000

    use just what we need and setup zend autoloader in symfony
    git-svn-id: http://svn.idci-consulting.fr/citadin.fr/trunk@114 9e24bf6e-2ee3-4532-97b1-3e928c6f3323

--- config/ProjectConfiguration.class.php
+++ config/ProjectConfiguration.class.php
@@ -5,6 +5,21 @@ sfCoreAutoload::register();
 
 class ProjectConfiguration extends sfProjectConfiguration
 {
+  static protected $zendLoaded = false;
+ 
+  static public function registerZend()
+  {
+    if (self::$zendLoaded)
+    {
+      return;
+    }
+ 
+    set_include_path(sfConfig::get('sf_lib_dir').'/vendor'.PATH_SEPARATOR.get_include_path());
+    require_once sfConfig::get('sf_lib_dir').'/vendor/Zend/Autoloader.php';
+    Zend_Loader_Autoloader::getInstance();
+    self::$zendLoaded = true;
+  }
+	
   public function setup()
   {
     // for compatibility / remove and enable only the plugins you want
