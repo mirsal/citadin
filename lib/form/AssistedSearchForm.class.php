b45d4da0e952b732b4409516e7ecabf3e77c88e1
mirsal <mirsal@9e24bf6e-2ee3-4532-97b1-3e928c6f3323>
2010-03-27 18:32:34 +0000

    Add the assisted search form class
    git-svn-id: http://svn.idci-consulting.fr/citadin.fr/trunk@122 9e24bf6e-2ee3-4532-97b1-3e928c6f3323

---
+++ lib/form/AssistedSearchForm.class.php
@@ -0,0 +1,27 @@
+<?php
+
+class AssistedSearchRequestForm extends sfForm
+{
+    public function configure()
+    {
+        $this->widgetSchema = new sfWidgetFormSchema(array(
+            'name' => new sfWidgetFormInput(),
+            'contact' => new sfWidgetFormInput(),
+            'message' => new sfWidgetFormTextArea()
+        ));
+
+        $this->validatorSchema = new sfValidatorSchema(array(
+            'name' => new sfValidatorString(array('required' => 'true')),
+            'contact' => new sfValidatorString(array('required' => 'true')),
+            'message' => new sfValidatorString(array('required' => 'true'))
+        ));
+
+        $this->getWidgetSchema()->setDefaults(array(
+            'name' => 'Votre nom',
+            'contact' => 'Votre e-mail ou téléphone',
+            'message' => 'Décrivez ce que vous recherchez, nous nous occupons du reste'
+        ));
+
+        $this->getWidgetSchema()->setNameFormat('assisted_search_request[%s]');
+    }
+}
