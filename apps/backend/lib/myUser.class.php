f3469c07282eff85906c9d4cf492eea5e97c6cef
mirsal <mirsal@9e24bf6e-2ee3-4532-97b1-3e928c6f3323>
2010-03-27 12:11:48 +0000

    Enable internationalization and force the fr locale
    git-svn-id: http://svn.idci-consulting.fr/citadin.fr/trunk@98 9e24bf6e-2ee3-4532-97b1-3e928c6f3323

--- apps/backend/lib/myUser.class.php
+++ apps/backend/lib/myUser.class.php
@@ -2,4 +2,9 @@
 
 class myUser extends sfGuardSecurityUser
 {
+    public function initialize(sfEventDispatcher $dispatcher, sfStorage $storage, $options = array())
+    {
+        parent::initialize($dispatcher, $storage, $options);
+        $this->setCulture(sfConfig::get('sf_default_culture', 'fr'));
+    }
 }
