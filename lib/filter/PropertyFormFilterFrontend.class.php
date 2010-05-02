03c165ad0f3232c67a7d75d911255c7e9f0f9e5b
Mirsal Ennaime <mirsal.ennaime@gmail.com>
2010-05-02 12:46:48 +0200

    Fix whitespace

--- lib/filter/PropertyFormFilterFrontend.class.php
+++ lib/filter/PropertyFormFilterFrontend.class.php
@@ -7,9 +7,9 @@ class PropertyFormFilterFrontend extends PropertyFormFilter
         parent::configure();
         $this->widgetSchema['type'] = new sfWidgetFormSelect(array(
             'choices' => array_merge(PropertyPeer::getTypes(), array('TYPE_ANY' => 'Peu importe'))
-  	    ));
+        ));
 
-  	    $this->getWidgetSchema()->setDefault('type', 'TYPE_ANY');
+        $this->getWidgetSchema()->setDefault('type', 'TYPE_ANY');
 
         $this->widgetSchema['price']->setOption('template',
             '<label>Min</label> %from% <label>Max</label> %to%');
